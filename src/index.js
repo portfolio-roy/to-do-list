b867132184288d20304a03738dc040842d8dc7cf
Utsargo Roy <royutsargo@gmail.com>
2022-12-10 09:16:47 +0600

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 /* eslint-disable no-unused-vars */
 
-import _ from 'lodash';
-import './style.scss';
-import buildPage from './modules/buildpage.js';
+import _ from "lodash";
+import "./style.scss";
+import buildPage from "./modules/buildpage.js";
 
-import addSingle from './modules/addsingle.js';
-import updateDom from './modules/updatedom.js';
-import toDos from './modules/data.js';
+import addSingle from "./modules/addsingle.js";
+import updateDom from "./modules/updatedom.js";
+import toDos from "./modules/data.js";
 
-const refreshBtn = document.getElementById('refresh-btn');
-refreshBtn.addEventListener('click', () => updateDom());
-const addBtn = document.getElementById('add-btn');
-const toDoInput = document.getElementById('to-do-input');
-addBtn.addEventListener('click', () => {
+const refreshBtn = document.getElementById("refresh-btn");
+refreshBtn.addEventListener("click", () => updateDom());
+const addBtn = document.getElementById("add-btn");
+const toDoInput = document.getElementById("to-do-input");
+addBtn.addEventListener("click", () => {
   addSingle();
 });
-toDoInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
+toDoInput.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
     addBtn.click();
   }
 });
 
-const clearBtn = document.getElementById('clear-all');
-clearBtn.addEventListener('click', () => {
+const clearBtn = document.getElementById("clear-all");
+clearBtn.addEventListener("click", () => {
   toDos.clearAll();
 });
 window.onload = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'eval-source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-source-map",
   devServer: {
-    static: './docs',
+    static: "./docs",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
     clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.s(a|c)ss$/i,
         use: [
           // Creates `style` nodes from JS strings
-          'style-loader',
+          "style-loader",
           // Translates CSS into CommonJS
-          'css-loader',
+          "css-loader",
           // Compiles Sass to CSS
-          'sass-loader',
+          "sass-loader",
         ],
       },
     ],

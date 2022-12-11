46e1fb0a58398529ca76170a55aff8fe2169556e
Utsargo Roy <royutsargo@gmail.com>
2022-12-11 14:25:09 +0600

    Change colors of list items

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sed -i '1s;^;/* eslint-disable */\n;' docs/main.js
 npx eslint . --fix
 npx stylelint "**/*.{css,scss}" --fix
 git add *
-read -d "~" -p 'Commit Message: ' commitMessage
+read -p 'Commit Message: ' commitMessage
 git commit -m "$commitMessage"
 read -p 'Branch: ' branchName
 git push -u origin $branchName

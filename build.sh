133452f9131b4d5dada60282889a8febdca874c8
Utsargo Roy <royutsargo@gmail.com>
2022-12-11 13:54:11 +0600

    Test script: - Changed style of clear all

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sed -i '1s;^;/* eslint-disable */\n;' docs/main.js
 npx eslint . --fix
 npx stylelint "**/*.{css,scss}" --fix
 git add *
-read -p 'Commit Message: ' commitMessage
+read -d "~" -p 'Commit Message: ' commitMessage
 git commit -m "$commitMessage"
 read -p 'Branch: ' branchName
 git push -u origin $branchName
